da8b9b9f5c7cc47b06d930428168e54d5ab49841
merodeadorNocturno <merodeador.nocturno@gmail.com>
2026-05-09 15:28:12 -0600

    chore: remove unused dependencies and cleanup logs

--- src/config/connection.rs
+++ src/config/connection.rs
@@ -1,12 +1,12 @@
 use dotenv::dotenv;
-use log::{info, warn};
+use log::warn;
 use std::env;
 
 pub fn set_environment_variable(env_var: &str, default_value: &str) -> String {
     dotenv().ok();
     match env::var(env_var) {
         Ok(var_in_env_file) => {
-            info!("Loaded {} env var", &env_var);
+            // info!("Loaded {} env var", &env_var);
             var_in_env_file
         }
         Err(e) => {
