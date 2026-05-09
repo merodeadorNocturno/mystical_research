e11365d90d8268a8dab3bd23871cf617e6119f35
merodeadorNocturno <merodeador.nocturno@gmail.com>
2026-05-09 14:29:04 -0600

    Build cross platform and copy to production

---
+++ build_alma_linux.sh
@@ -0,0 +1,1 @@
+cargo zigbuild --target aarch64-unknown-linux-gnu.2.34 --release
---
+++ copy_exe.sh
@@ -0,0 +1,1 @@
+scp -i ~/.ssh/ssh-key-2024-05-13.key ./target/aarch64-unknown-linux-gnu/release/angels opc@159.54.128.170:~/
